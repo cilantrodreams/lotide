c53ade77f876b601b5c13687d83957387e07c631
Thomas Ngo <thomasngo818@gmail.com>
2022-01-11 01:01:00 +0000

    Implemented custom assert function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEquals("Lighthouse Labs", "Bootcamp");
+assertEquals(1, 1);
+assertEquals("Hello world", "Hello world");
+assertEquals(1, 4);
