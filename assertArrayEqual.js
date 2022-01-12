0eab9fbcad685904f4433f9f49a0e860ca7848ed
Thomas Ngo <thomasngo818@gmail.com>
2022-01-12 03:19:23 +0000

    Added assertArrayEqual.js file with function to compare two arrays

---
+++ assertArrayEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(actualArray, expectedArray) {
+  let arrayMatch = true;
+  if (actualArray.length !== expectedArray.length) {
+    arrayMatch = false;
+  }
+  for (let i = 0; i < expectedArray.length; i++) {
+    if (expectedArray[i] !== actualArray[i]) {
+      arrayMatch = false;
+    }
+  }
+  return arrayMatch;
+}
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertArrayEqual([1, 2, 3], ['1', '2', '3']);
