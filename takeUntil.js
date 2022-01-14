17dae1013dc41c0c8f39f75e7c6fd547076595ba
Thomas Ngo <thomasngo818@gmail.com>
2022-01-14 06:21:40 +0000

    added takeUntil.js with function to slice array from beginning to callback truthy output

---
+++ takeUntil.js
@@ -0,0 +1,45 @@
+const eqArrays = function(actualArray, expectedArray) {
+  let arrayMatch = true;
+  if (actualArray.length !== expectedArray.length) {
+    arrayMatch = false;
+  }
+  for (let i = 0; i < expectedArray.length; i++) {
+    if (expectedArray[i] !== actualArray[i]) {
+      arrayMatch = false;
+    }
+  }
+  return arrayMatch;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// return a slice of the array with elements taken from the beginning
+// the slice starts at the beginning of the array keeps going until the callback returns truthy
+// callback accepts one value, which is the item to end the slice at.
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+// test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
+
+const data3 = ['The', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog'];
+assertArraysEqual(takeUntil(data3, x => x.length === 6), ['The', 'quick', 'brown', 'fox']);
