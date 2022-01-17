9435c7e51d212d58e398514a9b0bbf4860557c3e
Thomas Ngo <thomasngo818@gmail.com>
2022-01-17 23:20:38 +0000

    refactored to use exports and require. Broke out test cases into their own files

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello world", "Hello world");
-assertEqual(1, 4);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello world", "Hello world");
+assertEqual(1, 4);
