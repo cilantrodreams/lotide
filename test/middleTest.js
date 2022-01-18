d0de91b90070ef836262818d83475d2d178840e1
Thomas Ngo <thomasngo818@gmail.com>
2022-01-18 03:36:06 +0000

    refactored middleTest.js to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,25 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle(['one', 'two', 'three', 'four', 'five']), ['three']);
-assertArraysEqual(middle(['one', 'two']), []);
-assertArraysEqual(middle(['one']), []);
-assertArraysEqual(middle(['one', 'two', 'three', 'four']), ['two', 'three']);
-assertArraysEqual(middle(['one', 'two', 'three', 'four', 'five', 'six']), ['three', 'four']);
-assertArraysEqual(middle([1, 2, 3, 'four', 'five', 'six']), [3, 'four']);
+describe('#middle', () => {
+  it("should return ['three'] for ['one', 'two', 'three', 'four', 'five']", () => {
+    const actual = middle(['one', 'two', 'three', 'four', 'five']);
+    assert.deepEqual(actual, ['three']);
+  });
+  it("should return [] for ['one', 'two']", () => {
+    const actual = middle(['one', 'two']);
+    assert.deepEqual(actual, []);
+  });
+  it("should return [] for ['one']", () => {
+    const actual = middle(['one']);
+    assert.deepEqual(actual, []);
+  });
+  it("should return ['two', 'three'] for ['one', 'two', 'three', 'four']", () => {
+    const actual = middle(['one', 'two', 'three', 'four']);
+    assert.deepEqual(actual, ['two', 'three']);
+  });
+  it("should return [3, 'four'] for [1, 2, 3, 'four', 'five', 'six']", () => {
+    const actual = middle([1, 2, 3, 'four', 'five', 'six']);
+    assert.deepEqual(actual, [3, 'four']);
+  });
+});
