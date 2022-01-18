e564b375a5ea0095b6ef58ed7ed2d114da56da20
Thomas Ngo <thomasngo818@gmail.com>
2022-01-18 04:39:27 +0000

    updated index to remove non utility functions

--- index.js
+++ index.js
@@ -2,13 +2,8 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
 const findKeyByValue = require('./findKeyByValue');
 const findKey = require('./findKey');
 const letterPositions = require('./letterPositions');
@@ -20,13 +15,8 @@ module.exports = {
   head,
   tail,
   middle,
-  assertArraysEqual,
-  assertEqual,
-  assertObjectsEqual,
   countLetters,
   countOnly,
-  eqArrays,
-  eqObjects,
   findKeyByValue,
   findKey,
   letterPositions,
